a0c78523bb767aee9b82f979ddc8b02b09f191e5
ernestopi <90855907+ernestopi@users.noreply.github.com>
2022-04-18 11:06:40 -0300

    Add files via upload

---
+++ SEM06-T02-Q01.py
@@ -0,0 +1,13 @@
+a = str(input('')) .lower()
+if a==('a'):
+    print('True')
+elif a == ('e'):
+    print('True')
+elif a==('i'):
+    print ('True')
+elif a==('o'):
+    print('True')
+elif a==('u'):
+    print('True')
+else:
+    print('False')
---
+++ SEM06-T02-Q02.py
@@ -0,0 +1,5 @@
+a = input() .upper()
+if 'A'<= a <='Z':
+    print ('True')
+else:
+    print('False')
